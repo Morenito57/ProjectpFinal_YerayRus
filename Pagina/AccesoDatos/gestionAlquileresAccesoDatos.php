86c34fe00f5085c18a5e933ca3db0120bd808613
Morenito57 <yerayrusmartinez@gmail.com>
2023-05-27 19:53:44 +0200

    la funcion de añadir dias de alquiler añadido

--- Pagina/AccesoDatos/gestionAlquileresAccesoDatos.php
+++ Pagina/AccesoDatos/gestionAlquileresAccesoDatos.php
@@ -18,6 +18,11 @@
 
             $estado = true;
 
+            $IdVehiculo = mysqli_real_escape_string($conexion, $IdVehiculo);
+            $FechaInicio = mysqli_real_escape_string($conexion, $FechaInicio);
+            $FechaFinal = mysqli_real_escape_string($conexion, $FechaFinal);
+            $TotalDelPrecio = mysqli_real_escape_string($conexion, $TotalDelPrecio);
+
             $consultaSaldo = mysqli_prepare($conexion, "SELECT Saldo FROM Usuario WHERE NombreUsuario = ?;");
             $consultaSaldo->bind_param("s", $IdUser);
             $consultaSaldo->execute();
@@ -45,12 +50,14 @@
 
             $consulta3 = mysqli_prepare($conexion, "INSERT INTO Alquiler_Seguro (Alquiler_id, Seguro_id) VALUES (?, ?)");
                 foreach ($IdSeguro as $Seguro) {
+                    $Seguro = mysqli_real_escape_string($conexion, $Seguro);
                     $consulta3->bind_param("ii", $alquiler_id, $Seguro);
                     $consulta3->execute();
                 }
 
             $consulta4 = mysqli_prepare($conexion, "INSERT INTO Alquiler_Extra (Alquiler_id, Extra_id) VALUES (?, ?)");
-                foreach ($IdExtra as $Extra) {          
+                foreach ($IdExtra as $Extra) {  
+                    $Extra = mysqli_real_escape_string($conexion, $Extra);        
                     $consulta4->bind_param("ii", $alquiler_id, $Extra);
                     $consulta4->execute();
                 }
@@ -80,7 +87,7 @@
 
             $usuarioOriginal = $_SESSION['usuario'];
 
-            $consulta = mysqli_prepare($conexion, "SELECT Alquiler.Id AS IdAlquiler, Vehiculo.Nombre AS NombreVehiculo, Alquiler.FechaInicio, Alquiler.FechaFinal, Cargo.FechaDevuelto, GROUP_CONCAT(DISTINCT Seguros.Seguro) AS Seguros, GROUP_CONCAT(DISTINCT Extras.Extra) AS Extras, Alquiler.TotalDelPrecio, Cargo.TotalCargo, Alquiler.Estado AS ActivoAlquiler, Cargo.Pagado, Cargo.Activo AS ActivoCargo FROM Alquiler LEFT JOIN Vehiculo ON Alquiler.IdVehiculo = Vehiculo.Id LEFT JOIN Cargo ON Alquiler.Id = Cargo.Alquiler_id LEFT JOIN Alquiler_Seguro ON Alquiler.Id = Alquiler_Seguro.Alquiler_id LEFT JOIN Seguros ON Alquiler_Seguro.Seguro_id = Seguros.Id LEFT JOIN Alquiler_Extra ON Alquiler.Id = Alquiler_Extra.Alquiler_id LEFT JOIN Extras ON Alquiler_Extra.Extra_id = Extras.Id WHERE Alquiler.IdUser = ? GROUP BY Alquiler.Id;");
+            $consulta = mysqli_prepare($conexion, "SELECT Alquiler.Id AS IdAlquiler, Vehiculo.Nombre AS NombreVehiculo, Vehiculo.Precio AS PrecioVehiculo, Alquiler.FechaInicio, Alquiler.FechaFinal, Cargo.FechaDevuelto, GROUP_CONCAT(DISTINCT Seguros.Seguro) AS Seguros, GROUP_CONCAT(DISTINCT Extras.Extra) AS Extras, Alquiler.TotalDelPrecio, Cargo.Id AS IdCargo, Cargo.TotalCargo, Alquiler.Estado AS ActivoAlquiler, Cargo.Pagado, Cargo.Activo AS ActivoCargo FROM Alquiler LEFT JOIN Vehiculo ON Alquiler.IdVehiculo = Vehiculo.Id LEFT JOIN Cargo ON Alquiler.Id = Cargo.Alquiler_id LEFT JOIN Alquiler_Seguro ON Alquiler.Id = Alquiler_Seguro.Alquiler_id LEFT JOIN Seguros ON Alquiler_Seguro.Seguro_id = Seguros.Id LEFT JOIN Alquiler_Extra ON Alquiler.Id = Alquiler_Extra.Alquiler_id LEFT JOIN Extras ON Alquiler_Extra.Extra_id = Extras.Id WHERE Alquiler.IdUser = ? GROUP BY Alquiler.Id;");
             
             $consulta->bind_param("s", $usuarioOriginal);
             $consulta->execute();
@@ -95,5 +102,106 @@
             }
             return $alquileres;
         }
+
+        function actualizarAlquiler($usuario, $IdAlquiler, $FechaFinal, $TotalPago) {
+
+            $conexion = mysqli_connect('localhost','root','');
+
+            if (mysqli_connect_errno())
+            {
+                    echo "Error al conectar a MySQL: ". mysqli_connect_error();
+            }
+
+            mysqli_select_db($conexion, 'LegendaryMotorsport');
+
+            $IdAlquiler = mysqli_real_escape_string($conexion, $IdAlquiler);
+            $FechaFinal = mysqli_real_escape_string($conexion, $FechaFinal);
+            $TotalPago = mysqli_real_escape_string($conexion, $TotalPago);
+
+            $consultaSaldo = mysqli_prepare($conexion, "SELECT Saldo FROM Usuario WHERE NombreUsuario = ?;");
+            $consultaSaldo->bind_param("s", $usuario);
+            $consultaSaldo->execute();
+
+            $resSaldo = $consultaSaldo->get_result();
+            $filaSaldo = $resSaldo->fetch_assoc();
+            $saldoActual = $filaSaldo['Saldo'];
+
+            if ($saldoActual < $TotalPago) {
+                $protocolo = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
+                $url = "$protocolo://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                header("Location: Area_Personal_Saldo_Usuario_Vista.php");
+                exit();
+            }
+
+            $consulta2 = mysqli_prepare($conexion, "UPDATE Usuario set Saldo = Saldo - (?) WHERE NombreUsuario = (?);");
+            $consulta2->bind_param("is", $TotalPago, $usuario);
+            $consulta2->execute();
+
+            $consulta3 = mysqli_prepare($conexion, "UPDATE Alquiler set FechaFinal = ? WHERE Id = (?);");
+            $consulta3->bind_param("si", $FechaFinal, $IdAlquiler);
+            $consulta3->execute();
+
+            $consulta4 = mysqli_prepare($conexion, "UPDATE Alquiler set TotalDelPrecio = TotalDelPrecio + ? WHERE Id = (?);");
+            $consulta4->bind_param("ii", $TotalPago, $IdAlquiler);
+            $consulta4->execute();
+
+            header("Location: Area_Personal_Gestion_Alquiler_Usuario_Vista.php?id=".urlencode($IdAlquiler));
+
+        }
+
+        function obtenerAlquiler($id) {
+
+            $conexion = mysqli_connect('localhost','root','');
+
+            if (mysqli_connect_errno())
+            {
+                    echo "Error al conectar a MySQL: ". mysqli_connect_error();
+            }
+
+            mysqli_select_db($conexion, 'LegendaryMotorsport');
+
+            $consulta = mysqli_prepare($conexion, "SELECT Alquiler.Id AS IdAlquiler, Vehiculo.Nombre AS NombreVehiculo, Vehiculo.Precio AS PrecioVehiculo, Alquiler.FechaInicio, Alquiler.FechaFinal, Cargo.FechaDevuelto, GROUP_CONCAT(DISTINCT Seguros.Seguro) AS Seguros, GROUP_CONCAT(DISTINCT Extras.Extra) AS Extras, Alquiler.TotalDelPrecio, Cargo.Id AS IdCargo, Cargo.TotalCargo, Alquiler.Estado AS ActivoAlquiler, Cargo.Pagado, Cargo.Activo AS ActivoCargo FROM Alquiler LEFT JOIN Vehiculo ON Alquiler.IdVehiculo = Vehiculo.Id LEFT JOIN Cargo ON Alquiler.Id = Cargo.Alquiler_id LEFT JOIN Alquiler_Seguro ON Alquiler.Id = Alquiler_Seguro.Alquiler_id LEFT JOIN Seguros ON Alquiler_Seguro.Seguro_id = Seguros.Id LEFT JOIN Alquiler_Extra ON Alquiler.Id = Alquiler_Extra.Alquiler_id LEFT JOIN Extras ON Alquiler_Extra.Extra_id = Extras.Id WHERE Alquiler.Id = ? GROUP BY Alquiler.Id;");
+            
+            $consulta->bind_param("s", $id);
+            $consulta->execute();
+            $result = $consulta->get_result();
+    
+            $alquileres =  array();
+    
+            while ($myrow = $result->fetch_assoc()) 
+            {
+                array_push($alquileres,$myrow);
+    
+            }
+            return $alquileres;
+            exit();
+        }
+
+        function actualizacion(){
+
+            $conexion = mysqli_connect('localhost','root','');
+
+            if (mysqli_connect_errno())
+            {
+                    echo "Error al conectar a MySQL: ". mysqli_connect_error();
+            }
+
+            mysqli_select_db($conexion, 'LegendaryMotorsport');
+
+            $consulta = mysqli_prepare($conexion, "SELECT Alquiler.Id AS IdAlquiler, Vehiculo.Nombre AS NombreVehiculo, Vehiculo.Precio AS PrecioVehiculo, Alquiler.FechaInicio, Alquiler.FechaFinal, Cargo.FechaDevuelto, GROUP_CONCAT(DISTINCT Seguros.Seguro) AS Seguros, GROUP_CONCAT(DISTINCT Extras.Extra) AS Extras, Alquiler.TotalDelPrecio, Cargo.Id AS IdCargo, Cargo.TotalCargo, Alquiler.Estado AS ActivoAlquiler, Cargo.Pagado, Cargo.Activo AS ActivoCargo FROM Alquiler LEFT JOIN Vehiculo ON Alquiler.IdVehiculo = Vehiculo.Id LEFT JOIN Cargo ON Alquiler.Id = Cargo.Alquiler_id LEFT JOIN Alquiler_Seguro ON Alquiler.Id = Alquiler_Seguro.Alquiler_id LEFT JOIN Seguros ON Alquiler_Seguro.Seguro_id = Seguros.Id LEFT JOIN Alquiler_Extra ON Alquiler.Id = Alquiler_Extra.Alquiler_id LEFT JOIN Extras ON Alquiler_Extra.Extra_id = Extras.Id;");
+            $consulta->execute();
+            $result = $consulta->get_result();
+    
+            $alquileres =  array();
+    
+            while ($myrow = $result->fetch_assoc()) 
+            {
+                array_push($alquileres,$myrow);
+    
+            }
+            return $alquileres;
+        }
+
+
     }
 ?>
