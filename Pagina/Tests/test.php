e1e64c0f4eb68519c870ae1c21438be1f226e47b
Morenito57 <yerayrusmartinez@gmail.com>
2023-06-06 23:25:20 +0200

    todo

--- Pagina/Tests/test.php
+++ Pagina/Tests/test.php
@@ -1,21 +1,37 @@
 <?php
-
-require("AccesoDatos/usuarioAccesoDatos.php");
-
+require("D:\Clase\laragon\laragon\www\TrabajoFinal\Pagina\AccesoDatos\vehiculoAccesoDatos.php");
+require("D:\Clase\laragon\laragon\www\TrabajoFinal\Pagina\AccesoDatos\tipoVehiculoAccesoDatos.php");
+require("D:\Clase\laragon\laragon\www\TrabajoFinal\Pagina\AccesoDatos\usuarioAccesoDatos.php");
 function test_alta_usuario()
 {
     $u = new UsuarioAccesoDatos();
-    return $u->insertar('yeray','12345678','Administrador');
+    //return $u->insertar('yes','12345a', 'yes', 'su', '2000-11-30', 'la calle', '46397584Y', '46397584Y', '601180116', 'yerayrusmartinez@gmail.com', 'otro');
 }
 
 function test_verificar_usuario_encontrado()
 {
-    $perfil_esperado = 'Administrador';
+    $perfil_esperado = 'Normal';
     $u = new UsuarioAccesoDatos();
-    $perfil = $u->verificar('yeray','12345678');
+    $perfil = $u->verificar('yes','12345a');
     return $perfil === $perfil_esperado;
 }
+
+function test_sacar_vehiculo()
+{
+    $u = new VehiculosAccesoDatos();
+    return $u->obtenerVehiculoConcreto(1);
+}
+
+function test_sacar_tipovehiculo()
+{
+    $u = new TipoVehiculoAccesoDatos();
+    return $u->obtenerTipoVehiculo(1);
+}
+
+
 
 
 var_dump(test_alta_usuario());
 var_dump(test_verificar_usuario_encontrado());
+var_dump(test_sacar_vehiculo());
+var_dump(test_sacar_tipovehiculo());
